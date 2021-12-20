ae0379568cfd26d5285ed4d66346db9df54b5a47
OshadhaShiro <shirokumara49@gmail.com>
2021-12-20 20:16:23 +0530

    Setup spotify.js

---
+++ src/spotify.js
@@ -0,0 +1,30 @@
+// https://developer.spotify.com/documentation/web-playback-sdk/quick-start/#
+
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+const redirectUri = 'https://spotify-clone-6a31a.web.app/';
+const clientId = '1b842d13d70c4bed90299282b5b3a044';
+
+const scopes = [
+	'user-read-currently-playing',
+	'user-read-recently-playing',
+	'user-read-playback-state',
+	'user-top-read',
+	'user-modify-playback-state',
+];
+
+export const getTokenFromUrl = () => {
+	return window.location.hash
+		.substring(1)
+		.split('&')
+		.reduce((initial, item) => {
+			// accessToken=mysupersecretkey&name=shiro
+			let parts = item.split('=');
+			initial[parts[0]] = decodeURIComponent(parts[1]);
+
+			return initial;
+		}, {});
+};
+
+export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope${scopes.join(
+	'%20',
+)}&response_type=token&show_dialog=true`;
